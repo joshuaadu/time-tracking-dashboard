8ae87000fc1188dfd1df6f71285f55abf7827e04
Joshua Adu-Mensah (Qwaben Josh) <joshuaadu2001@gmail.com>
2022-03-06 00:23:43 +0000

    Complete Activity component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import { useState } from "react";
 import Activity from "./components/Activity";
 import Profile from "./components/Profile";
+import Data from "./store/data";
 import "./styles.css";
 
 export default function App() {
+  const [timeFrame, setTimeFrame] = useState("weekly");
+
   const data = ["play", "worK", "Study", "exercise", "Self-care"];
   return (
     <div className="App">
       <Profile />
-      {data.map((item) => (
-        <Activity title={item} key={item} />
+      {Data.map((item) => (
+        <Activity
+          title={item.title}
+          data={item}
+          key={item.title}
+          timeFrame={timeFrame}
+        />
       ))}
     </div>
   );
